63002a9cb45282ba7a2396e05d9177f2cc90b468
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-10-11 12:49:49 +0600

    update

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/user', userRouter)
 app.use('/api/dashboard',user)
 
 
-app.listen(port, () =>{
+app.listen(process.env.PORT || port, () =>{
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
